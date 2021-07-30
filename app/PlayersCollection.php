8a70ffe27619e69633795ef6cd23c9b437539d83
Andy Wendt <andy@awendt.com>
2021-07-29 21:34:25 -0600

    Add return types

--- app/PlayersCollection.php
+++ app/PlayersCollection.php
@@ -6,18 +6,18 @@ use Illuminate\Database\Eloquent\Collection;
 
 class PlayersCollection extends Collection
 {
-    public function goalies()
+    public function goalies(): self
     {
         return $this->filter(fn($player) => $player->isGoalie)->sortByDesc('ranking');
     }
 
-    public function ranked()
+    public function ranked(): self
     {
         $goalieIds = $this->goalies()->pluck('id')->all();
         return $this->whereNotIn('id', $goalieIds)->sortByDesc('ranking');
     }
 
-    public function sorted()
+    public function sorted(): self
     {
         return $this->goalies()->concat($this->ranked());
     }
